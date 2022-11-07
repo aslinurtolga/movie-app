797d2e8878ffedb2b00865ea1422dde9b763e807
AslinurTolga <aslinurtolga@gmail.com>
2022-11-07 22:38:40 +0300

    login page done

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -1,7 +1,11 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 
 //* Your web app's Firebase configuration
+// TODO: Replace the following with your app's Firebase project configuration
+//* https://firebase.google.com/docs/auth/web/start
+//* https://console.firebase.google.com/ => project settings
+//! firebase console settings bölümünden firebaseconfig ayarlarını al
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_apiKey,
   authDomain: process.env.REACT_APP_authDomain,
@@ -15,3 +19,17 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
+
+export const createUser = async ({ email, password }) => {
+  //? yeni bir kullanıcı oluşturmak için kullanılan firebase methodu
+  try {
+    let userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    console.log(userCredential);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
